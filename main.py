75141cc3ad52411b99f610d264bc93936a47ca26
valmsmith39a <>
2023-04-09 05:32:41 +0000

    GET request to OpenAI's Chat Completion API

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+import os 
+import openai 
 from fastapi import FastAPI
+
+openai.api_key = os.environ['CHATGPT_API_KEY']
 
 app = FastAPI()
 
-# Default root endpoint
 @app.get("/")
 async def root():
-  return { "message": "Hello world" }
+	return { "message": "Hello Metaverse" }
 
-# Example path parameter
-@app.get("/name/{name}")
-async def name(name: str):
-  return { "message": f"Hello {name}" }
+@app.get("/sentiment/emotion-detection")
+async def get_sentiment_emotion_detection(text: str):
+	messages = [
+		{"role": "system", "content": "You are a helpful assistant."},
+		{"role": "user", "content": "Who won the world series in 2020?"},
+		{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+		{"role": "user", "content": "Where was it played?"}
+	]
+	response = call_chatgpt(messages)
+	if 'choices' in response:
+		assistant_response = response['choices'][0]['message']['content']
+		print(f"Assistant: {assistant_response}")
+	else: 
+		print("Error: API response is not as expected")
+
+	return { "message": "Hello from sentiment emotion-detection" }
+	
+@app.get("/twitter/tweets/summary")
+async def get_tweets_summary(text: str):
+	
+	return { "message": f"User text input is: {text}"}
+	
+def call_chatgpt(messages):
+	response = openai.ChatCompletion.create(
+		model="gpt-3.5-turbo",
+		messages=messages
+	)
+	return response 
+	
